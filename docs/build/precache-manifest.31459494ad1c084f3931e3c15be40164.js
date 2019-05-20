c8c0d95a78a3db0ff24ed9a2dbe342310ed8585c
superanika <anamaria.romerogarcia@gmail.com>
2019-05-20 19:42:22 +0200

    create docs

---
+++ docs/build/precache-manifest.31459494ad1c084f3931e3c15be40164.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9fac704229c82a064bd18c9b0ea17b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcdb0077db519fe6b4fd",
+    "url": "/static/css/main.1281761f.chunk.css"
+  },
+  {
+    "revision": "9f0f47cc047dd2ddec08",
+    "url": "/static/js/2.3d3e5e3a.chunk.js"
+  },
+  {
+    "revision": "dcdb0077db519fe6b4fd",
+    "url": "/static/js/main.ef433290.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3f8b5045006173b8ca1a981cbeb860dd",
+    "url": "/static/media/repelente.3f8b5045.png"
+  }
+]);
