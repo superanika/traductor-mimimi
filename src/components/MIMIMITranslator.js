6d065b6caba8ac33de8266c8ba1c89cafdb1eb78
superanika <anamaria.romerogarcia@gmail.com>
2019-05-22 18:32:29 +0200

    mobile view

--- src/components/MIMIMITranslator.js
+++ src/components/MIMIMITranslator.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './MIMIMITranslator.scss';
-import girl from '../repelente.png';
 
 class MIMIMITranslator extends React.Component {
     render() {
         return (
             <div className="container">
-                <img src={girl} alt="girl" className="girl"/>
+                <div className="girl"></div>
                 <div className={(this.props.mimimi !== '') ? 'bocadillo' : 'bocadillo hidden' }>
                     <p className='translation'>{this.props.mimimi}</p>
                 </div>
